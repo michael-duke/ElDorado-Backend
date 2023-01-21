1d891fc8001f7dc422a9221eb08609edfcbcf648
michael-duke <michael.21duke@gmail.com>
2023-01-21 16:51:53 +0300

    Remove belongs_to :user from car model - Add admin role to user model, and add admin? method Remove car ownership validation from booking model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,6 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :cars, through: :bookings, dependent: :destroy
 
-  def owner_cars
-    Car.where(user: self)
-  end
-
   def admin?
     role == 1
   end
