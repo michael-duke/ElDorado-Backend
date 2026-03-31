61457d5fd640455d0d37ae987d56da82de02978a
Michael Mesfin <michael.21duke@gmail.com>
2026-03-31 14:26:25 -0400

    Sort and fix scope and namespace for admin and customer routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,36 @@
+# config/routes.rb
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
   scope :api, defaults: { format: :json } do
     scope :v1 do
-      # Auth Routes
-      devise_for :users,
-        path: '',
-        path_names: {
-          sign_in: 'login',
-          sign_out: 'logout',
-          registration: 'register'
-        },
-        controllers: {
-          registrations: "api/v1/users/registrations",
-          sessions: "api/v1/users/sessions"
-        }
+      # --- Auth Routes ---
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login', sign_out: 'logout', registration: 'register'
+      }, controllers: {
+        registrations: "api/v1/users/registrations",
+        sessions: "api/v1/users/sessions"
+      }
 
+      # --- Public/Customer Routes ---
       scope module: 'api/v1' do
-        # Business Routes
         resources :reservations, only: [:index, :create, :destroy]
+        resources :cars, only: [:index, :show]
+        get 'profile', to: 'users#profile'
         
-        resources :cars, only: [:index, :show, :create, :update] do
-          member do
-            patch :availability # PATCH /api/v1/cars/:id/availability
+        # --- Admin/Owner Routes ---
+        namespace :admin do
+          resources :cars do
+            member do
+              patch :availability # PATCH /api/v1/admin/cars/:id/availability
+            end
+            # GET /api/v1/admin/cars/:car_id/status_histories
+            resources :status_histories, only: [:index], controller: 'car_status_histories'
           end
         end
-        
-        get 'profile', to: 'users#profile'
-        get 'all_cars', to: 'cars#all_cars'
       end
+
     end
   end
 end
